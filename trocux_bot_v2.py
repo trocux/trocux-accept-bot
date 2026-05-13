f3d1447c6f290ee81af2c1ae788a48fb9127fe40
trocux <trocux@gmail.com>
2026-05-13 02:35:23 -0700

    Update trocux_bot_v2.py

--- trocux_bot_v2.py
+++ trocux_bot_v2.py
@@ -6,7 +6,7 @@ from telegram.ext import Application, ChatJoinRequestHandler, ContextTypes
 
 # ===== НАСТРОЙКИ =====
 import os
-BOT_TOKEN = os.environ.get("8923267714:AAEy1Cro9cDxlK9Aym3PtuNfa8-KvvXKaS8")
+BOT_TOKEN = os.environ.get("8923267714:AAG7FKAyLYoAwAjMFg2lk0RtYP6hkYXk5s4")
 DELAY_SECONDS = 2 * 60          # 2 минуты
 DAILY_LIMIT = 400               # Максимум одобрений в день
 # =====================
